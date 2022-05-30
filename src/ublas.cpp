7abeee2310d0cebda22a041747bb0757e3828d1c
NSOLONKO <nikita.solonko@volvocars.com>
2022-05-30 10:17:45 +0300

    Add files

---
+++ src/ublas.cpp
@@ -0,0 +1,23 @@
+//
+// Created by nsolonko on 29.05.22.
+//
+#include <iostream>
+
+#include "common.hpp"
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cout << "Wrong usage, should be 2 parameters\n";
+        return 1;
+    }
+
+    auto a = utils::ReadMatrixFromFile<utils::MatrixDataType>(argv[1]);
+    auto b = utils::ReadMatrixFromFile<utils::MatrixDataType>(argv[2]);
+
+    {
+        utils::Benchmark benchmark("Ublas");
+        utils::Matrix res = utils::prod(a, b);
+    }
+
+    return 0;
+}
